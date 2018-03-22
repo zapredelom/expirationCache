f115a31eb38985b25c23441795b569f8f73c41c9
Azat Manukyan <livingfordead@gmail.com>
2018-03-22 18:35:06 +0400

    minor changes in example.cpp

--- example.cpp
+++ example.cpp
@@ -11,5 +11,13 @@ std::this_thread::sleep_for(std::chrono::seconds(3));
 std::cout<<e1.Count()<<" "<<e2.Count()<<std::endl;
 std::this_thread::sleep_for(std::chrono::seconds(7));
 std::cout<<e1.Count()<<" "<<e2.Count()<<std::endl;
+try
+{
+	e1.Get(33);
+}
+catch(std::out_of_range&e)
+{
+	std::cout<<e.what()<<std::endl;
+}
 return 0;
 }
