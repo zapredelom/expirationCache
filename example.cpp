6b3d7c362f244cc7c3b516bb3dfc247cbb55a57e
Azat Manukyan <livingfordead@gmail.com>
2018-03-22 17:26:48 +0400

    added example

---
+++ example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "ExpirationCache.h"
+int main ()
+{
+ExpirationCache<int, int, 4> e1;
+ExpirationCache<int, int, 6> e2;
+e1.Put(1,1);
+e2.Put(1,1);
+std::cout<<e1.Count()<<" "<<e2.Count()<<std::endl;
+std::this_thread::sleep_for(std::chrono::seconds(3));
+std::cout<<e1.Count()<<" "<<e2.Count()<<std::endl;
+std::this_thread::sleep_for(std::chrono::seconds(7));
+std::cout<<e1.Count()<<" "<<e2.Count()<<std::endl;
+return 0;
+}
